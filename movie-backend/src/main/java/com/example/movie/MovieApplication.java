35f8dd7ad8aa3f8556ff42f90772d1ba5df9b78f
Tejas <tthakare73@gmail.com>
2023-10-04 08:02:31 +0530

    Added Developer hot reloading

--- movie-backend/src/main/java/com/example/movie/MovieApplication.java
+++ movie-backend/src/main/java/com/example/movie/MovieApplication.java
@@ -2,12 +2,19 @@ package com.example.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MovieApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String helloWorld(){
+		return "Hii There What's Up Guys";
+	}
 }
